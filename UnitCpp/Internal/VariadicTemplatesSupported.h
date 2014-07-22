48423a3409cd4c28afd3c3803fbdacf26158dfa8
David Corne <davidcorne@gmail.com>
2014-07-22 16:19:23 +0100

    Tidy VariadicTemplatesSupported.

--- UnitCpp/Internal/VariadicTemplatesSupported.h
+++ UnitCpp/Internal/VariadicTemplatesSupported.h
@@ -5,12 +5,9 @@
 #ifndef VariadicTemplatesSupported_H
 #define VariadicTemplatesSupported_H
 
-#define UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED 0
-
 // For using visual studio. Support is from Visual Studio 2013.
 #ifdef _MSC_VER
 #if _MSC_VER >= 1800 // Visual Studio 2013's internal code.
-#undef UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED
 #define UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED 1
 #endif // _MSC_VER >= 1800
 #endif // ifdef _MSC_VER
@@ -20,17 +17,19 @@
 // if it's 4.X
 #if __GNUC__ == 4
 #if __GNUC_MINOR__ >= 3
-#undef UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED
 #define UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED 1
 #endif // __GNUC_MINOR__ >= 3
 #endif // __GNUC__ == 4
 
 // later versions than 4.
 #if __GNUC__ > 4
-#undef UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED
 #define UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED 1
 #endif // __GNUC__ > 4
 
-#endif // __GNUC__
+#endif // ifdef __GNUC__
 
-#endif // VariadicTemplatesSupported_H
+#ifndef UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED
+#define UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED 0
+#endif // ifndef UNITCPP_INTERNALS_VARIADIC_TEMPLATES_SUPPORTED
+
+#endif // ifndef VariadicTemplatesSupported_H
