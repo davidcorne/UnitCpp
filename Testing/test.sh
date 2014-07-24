3af53c4bdab5dcbf30ce6822bcf10b540569e9b8
David Corne <davidcorne@gmail.com>
2014-07-24 09:13:05 +0100

    Add what it was tested with note to the end of test.sh.

--- Testing/test.sh
+++ Testing/test.sh
@@ -3,11 +3,13 @@
 
 set -e
 
+tested_compilers=""
 #==============================================================================
 if $(command -v clang >/dev/null 2>&1)
 then
   echo "Has clang."
   make test COMPILER_TYPE=clang
+  tested_compilers="$tested_compilers clang"
 fi
 
 #==============================================================================
@@ -15,6 +17,7 @@ if $(command -v cl >/dev/null 2>&1)
 then
   echo "Has cl."
   make test COMPILER_TYPE=vs
+  tested_compilers="$tested_compilers cl"
 fi
 
 #==============================================================================
@@ -22,4 +25,7 @@ if $(command -v g++ >/dev/null 2>&1)
 then
   echo "Has g++."
   make test COMPILER_TYPE=gcc
+  tested_compilers="$tested_compilers g++"
 fi
+
+echo "Tested using:$tested_compilers"
