c9046982c58cc91bbd22b9a8855d78cfb59ff396
David Corne <davidcorne@gmail.com>
2016-04-13 15:32:47 +0100

    Add an actual test to utest_StaticAssert
    --HG--
    branch : dev

--- Testing/source/utest_StaticAssert.cpp
+++ Testing/source/utest_StaticAssert.cpp
@@ -8,5 +8,14 @@
 //=============================================================================
 TEST(Wish, static_assert)
 {
-  TEST_STATIC((Fraction<1, 2>::Numerator == 1), "1/2 numerator is 1.");
+  TEST_STATIC(
+    (Fraction<1, 2>::Numerator == 1),
+    "Wish::static_assert 1/2 numerator is 1."
+  );
+  // Make sure that if it passes, it prints the message into the log.
+  std::stringstream ss;
+  display_results(ss);
+  size_t index = ss.str().find("Wish::static_assert 1/2 numerator is 1.");
+  TEST_NOT_EQUAL(index, std::string::npos);
+  
 }
