4fa8e584bbaabd02a21bcb77ad98b478aa93df1b
David Corne <davidcorne@gmail.com>
2016-05-06 10:38:23 +0100

    Change fraction to solve odd g++ compile error.
    --HG--
    branch : dev

--- Testing/Testing/Fraction.h
+++ Testing/Testing/Fraction.h
@@ -7,8 +7,14 @@
 //=============================================================================
 template <int N, int D>
 struct Fraction {
- static const int Numerator = N;
- static const int Denominator = D;
+  static const int Numerator = N;
+  static const int Denominator = D;
 };
+
+template <int N, int D>
+const int Fraction<N, D>::Numerator;
+
+template <int N, int D>
+const int Fraction<N, D>::Denominator;
 
 #endif
